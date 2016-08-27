30e485f5ba61e0749bd32f54fd542c6bfa5b84b7
Rosiliza Rioja <rrioja@yondu.com>
2016-08-27 14:05:29 +0800

    AppServiceProvider: register interfaces and repositories

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind('App\Contracts\UserInterface', 'App\Repositories\UserRepository');
+        $this->app->bind('App\Contracts\UserAccountInterface', 'App\Repositories\UserAccountRepository');
+        $this->app->bind('App\Contracts\ContributorInterface', 'App\Repositories\ContributorRepository');
+        $this->app->bind('App\Contracts\GoalInterface', 'App\Repositories\GoalRepository');
+        $this->app->bind('App\Contracts\TransactionInterface', 'App\Repositories\TransactionRepository');
     }
 }
